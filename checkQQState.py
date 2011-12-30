9adbaa99f3763cac37611a0074439cbd8f8857b0
YAOYAO WANG <yorathwang@gmail.com>
2011-12-30 22:05:36 +0800

    change to python 2.X

--- checkQQState.py
+++ checkQQState.py
@@ -1,31 +1,26 @@
-'''
-Created on 2011-5-24
+#!/usr/bin/env python
+import time,datetime
+import urllib2
 
-@author: wangyaoyao
-'''
-
-import datetime
-import urllib.request
-
-def chkqq(qqnum):
-    chkurl = 'http://wpa.qq.com/pa?p=1:{}:1'.format(qqnum)
-    page = urllib.request.urlopen(chkurl)
-    length = page.headers.get('content-length')
-    page.close()
-    print(datetime.datetime.now())
-    if length == '2329':
+def chk_qq(qqnum):
+    chkurl = 'http://wpa.qq.com/pa?p=1:'+`qqnum`+':1'
+    a = urllib2.urlopen(chkurl)
+    length=a.headers.get("content-length")
+    a.close()
+    print datetime.datetime.now()
+    if length=='2329':
         return 'Online'
-    elif length == '2262':
+    elif length=='2262':
         return 'Offline'
     else:
         return 'Unknown Status!'
 
 def main():
     while True:
-        num = input("Input the qq num: ")
+        num = raw_input("Input the qq num: ")
         if num == "q":
             return 0
-        print(chkqq(num))
+        print(chk_qq(num))
 
-if __name__ == '__main__': main()
-
+if __name__ == '__main__':
+	main()
